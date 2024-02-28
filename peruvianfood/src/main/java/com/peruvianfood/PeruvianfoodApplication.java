33e42b082d9693aeb133191ae0d78ac96aa34885
christoferB <christofer3012@gmail.com>
2024-02-28 23:26:08 +0100

    "Peruvianfood "

---
+++ peruvianfood/src/main/java/com/peruvianfood/PeruvianfoodApplication.java
@@ -0,0 +1,13 @@
+package com.peruvianfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeruvianfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeruvianfoodApplication.class, args);
+	}
+
+}
---
+++ peruvianfood/src/test/java/com/peruvianfood/PeruvianfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peruvianfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeruvianfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
